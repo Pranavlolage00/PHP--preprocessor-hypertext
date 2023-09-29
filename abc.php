730a775276b10c009b1ab572c542a82bd1a927da
Pranav G. Lolage <129136237+Pranavlolage00@users.noreply.github.com>
2023-09-29 23:58:37 +0530

    Add files via upload

--- abc.php
+++ abc.php
@@ -1,9 +1,22 @@
 <?php
-  $a=10;
-  $b=30;
-
-  $c=$a+$b;
-
-  echo("addition=".$c);
-
-?>
+  $con=mysqli_connect("localhost","root","");
+  if($con==false)
+  {
+     die("error in conection...");
+  }
+    mysqli_select_db($con,"sybbaca");
+    $k=mysqli_query($con,"create table employee(eno int primary key,
+    ename varchar(20),
+    addr varchar(20),
+    join_date date,
+    sal float)");
+    if($k==true)
+    {
+        echo("table created");
+    }
+    else
+    {
+        echo("table already exist..");
+    }
+    mysqli_close($con);
+    ?>
